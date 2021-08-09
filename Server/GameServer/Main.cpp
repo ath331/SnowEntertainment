ceb5f0212385999033dade4e989206722e6ba398
ath331 <kstnftk@naver.com>
2021-08-09 21:37:35 +0900

    [Server] 테스트 함수 삭제
    테스트용 SumNumber 함수 삭제

--- Server/GameServer/Main.cpp
+++ Server/GameServer/Main.cpp
@@ -2,18 +2,9 @@
 
 using namespace std;
 
-void SumNumber( int& a, int b )
-{
-	a += b;
-}
-
 int main()
 {
-	int a = 10;
-	int b = 5;
-
-	SumNumber( a, b );
-	cout << a << endl;
+	cout << "HelloWorld !!" << endl;
 
 	return 0;
 }
